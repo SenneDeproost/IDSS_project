7950198cfa7c2d58b91c0e67c7694d3d141a369e
Senne Deproost <sennedeproost@hotmail.com>
2019-11-27 11:12:00 +0100

    Added feature selection part

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+import hashlib
+import ctypes
+from sklearn.ensemble import ExtraTreesClassifier
+import matplotlib.pyplot as plt
 
 data_dirname = "./data/"
 data_filename = "data.csv"
 
 data = pd.read_csv(data_dirname+data_filename)
 
+#data = data.apply(pd.to_numeric)
+#data['facilityId'] = data['facilityId'].astype(int)
+#print (data.dtypes)
+
+#def hash(string):
+	#return abs(hash(string)) % (10 ** 8)
+
+
+for col in data:
+	data[col] = abs(data[col].apply(hash))
+
+
+#data["facilityId"] = data["facilityId"].apply(hash)
+
+
+# https://towardsdatascience.com/feature-selection-techniques-in-machine-learning-with-python-f24e7da3f36e
+target = 'totalCritical'
+X = data.loc[:, data.columns != target]  #independent columns
+y = data[target]    #target column i.e price range
+
+
+#apply SelectKBest class to extract top 10 best features
+bestfeatures = SelectKBest(score_func=chi2, k=20)
+fit = bestfeatures.fit(X,y)
+dfscores = pd.DataFrame(fit.scores_)
+dfcolumns = pd.DataFrame(X.columns)
+#concat two dataframes for better visualization 
+featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+print(featureScores.nlargest(20,'Score'))  #print 10 best features
+
+
+# ----------------------------------
+
+model = ExtraTreesClassifier()
+model.fit(X,y)
+print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
+#plot graph of feature importances for better visualization
+feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+feat_importances.nlargest(20).plot(kind='barh')
+plt.show()
 
