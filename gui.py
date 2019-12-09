38f8511f1b906d2d4e16639425b9db22f0dbf338
Senne Deproost <sennedeproost@hotmail.com>
2019-12-09 15:14:31 +0100

    Added R GUI

--- gui.py
+++ gui.py
@@ -1,33 +1,31 @@
 import sys
-
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QDialog
-from PyQt5.QtWidgets import QDialogButtonBox
-from PyQt5.QtWidgets import QFormLayout
-from PyQt5.QtWidgets import QLineEdit
-from PyQt5.QtWidgets import QVBoxLayout
-
-class Dialog(QDialog):
-    """Dialog."""
-    def __init__(self, parent=None):
-        """Initializer."""
-        super().__init__(parent)
-        self.setWindowTitle('QDialog')
-        dlgLayout = QVBoxLayout()
-        formLayout = QFormLayout()
-        formLayout.addRow('Name:', QLineEdit())
-        formLayout.addRow('Age:', QLineEdit())
-        formLayout.addRow('Job:', QLineEdit())
-        formLayout.addRow('Hobbies:', QLineEdit())
-        dlgLayout.addLayout(formLayout)
-        btns = QDialogButtonBox()
-        btns.setStandardButtons(
-            QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
-        dlgLayout.addWidget(btns)
-        self.setLayout(dlgLayout)
-
+ 
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QPushButton, QLineEdit
+from PySide2.QtCore import QFile, QObject
+ 
+class Form(QObject):
+ 
+    def __init__(self, ui_file, parent=None):
+        super(Form, self).__init__(parent)
+        ui_file = QFile(ui_file)
+        ui_file.open(QFile.ReadOnly)
+ 
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+ 
+        self.line = self.window.findChild(QLineEdit, 'lineEdit')
+ 
+        btn = self.window.findChild(QPushButton, 'pushButton')
+        btn.clicked.connect(self.ok_handler)
+        self.window.show()
+ 
+    def ok_handler(self):
+        language = 'None' if not self.line.text() else self.line.text()
+        print('Favorite language: {}'.format(language))
+ 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    dlg = Dialog()
-    dlg.show()
+    form = Form('g.ui')
     sys.exit(app.exec_())
